8bfedcacf53ae50afdbdf81b3b467e938cef4f69
Geoffrey GUERET <g.gueret@gmail.com>
2019-12-21 15:27:59 +0100

    customized config for extras and so on

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Geoffrey GUERET'
-SITENAME = 'Geoffrey GUERET'
+AUTHOR = 'Geoffrey Guéret'
+SITENAME = 'Geoffrey Guéret'
 SITEURL = ''
 
 PATH = 'content'
@@ -32,4 +32,43 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+# CUSTOMS
+THEME = "themes/third"
+
+STATIC_PATHS = ("extras",)
+EXTRA_PATH_METADATA = {
+    "extras/robots.txt": {"path": "robots.txt"},
+    "extras/favicon.ico": {"path": "favicon.ico"},
+    "extras/favicon.svg": {"path": "favicon.svg"},
+    "extras/android-chrome-192x192.png": {"path": "android-chrome-192x192.png"},
+    "extras/android-chrome-256x256.png": {"path": "android-chrome-256x256.png"},
+    "extras/android-chrome-512x512.png": {"path": "android-chrome-512x512.png"},
+    "extras/apple-touch-icon.png": {"path": "apple-touch-icon.png"},
+    "extras/browserconfig.xml": {"path": "browserconfig.xml"},
+    "extras/favicon-16x16.png": {"path": "favicon-16x16.png"},
+    "extras/favicon-32x32.png": {"path": "favicon-32x32.png"},
+    "extras/mstile-150x150.png": {"path": "mstile-150x150.png"},
+    "extras/safari-pinned-tab.svg": {"path": "safari-pinned-tab.svg"},
+    "extras/site.webmanifest": {"path": "site.webmanifest"}
+}
+
+DEFAULT_CATEGORY = "misc"
+USE_FOLDER_AS_CATEGORY = False
+
+MARKDOWN = {
+    "extension_configs": {
+        "markdown.extensions.codehilite": {"css_class": "highlight"},
+        "markdown.extensions.extra": {},
+        "markdown.extensions.meta": {},
+        "markdown.extensions.toc": {"permalink": "", "permalink_class": "headerlink fas fa-link"}
+    },
+    "output_format": "html5",
+}
+
+PLUGINS = ["plugins.assets"]
+
+ARTICLE_URL = ARTICLE_SAVE_AS = "{slug}.html"
+PAGE_URL = "{slug}/"
+PAGE_SAVE_AS = "{slug}/index.html"
