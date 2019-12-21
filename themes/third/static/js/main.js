ed21047c9e5468743657895f7f9d384ee7ff8371
Geoffrey GUERET <g.gueret@gmail.com>
2019-12-21 15:23:29 +0100

    added theme

---
+++ themes/third/static/js/main.js
@@ -0,0 +1,29 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function(event) {
+// Pick a random buzzword to kick some asses
+	var buzzwords = [
+		"hydroponics",
+		"IoT",
+		"tech"
+	];
+
+	var container = document.getElementById("buzzword")
+	// element content must be cleaned for typed.js
+	container.innerHTML = "";
+
+	new Typed("#buzzword", {
+		strings: [buzzwords[Math.floor(Math.random() * buzzwords.length)]],
+		typeSpeed: 30,
+		loop: false,
+		showCursor: true,
+		onComplete: setTimeout(function(){
+			document.querySelectorAll(".typed-cursor")[0].style.display = "none";
+
+			setTimeout(function(){
+				container.textContent += ".";
+			}, 30)();
+
+		}, 1000)
+	});
+});
